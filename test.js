aa842f52bcaa75b7da50be056a43050940433654
afternine9 <afternine9@gmail.com>
2022-05-28 13:04:23 +0900

    [ADD]open close principle

--- test.js
+++ test.js
@@ -19,39 +19,79 @@ function addEventListener(eventType, listener) {
 
 // main
 function main() {
+  new InfoToolTip();
+  new StateToolTip();
+  const makeCatClass = new makeCatButton();
+  const callCatFunc = callCat(makeCatClass);
+  new callCatBtn(callCatFunc);
+
+  makeCatClass.click();
+}
+
+function makeCat() {
   const $parent = document.getElementById("playground");
   const randomAge = getRandomNumber(1, 14);
   const randomName = CAT_NAME_LIST[randomAge % 6];
-  new Cat($parent, {
-    name: randomName,
+  const idx = getRandomNumber(0, 4);
+  const catType = [
+    Cat,
+    RussianBlue,
+    koreanShorthairCat,
+    BengalCat,
+    NorwegianForestCat,
+  ];
+  return new catType[idx]($parent, {
+    name: `${randomName}${new Date().getTime()}`,
     age: randomAge,
   });
-  new InfoToolTip();
-  new StateToolTip();
 }
 
 // cat class
 class Cat {
   constructor($parent, data) {
     const idx = getRandomNumber(1, 100) % 6;
-    const dataController = new CatData({ ...data, state: CAT_ACT[idx] });
-    this._domController = new CatDom($parent, dataController);
+    this._dataController = new CatData({ profile: data, state: CAT_ACT[idx] });
+    this._domController = new CatDom($parent, this);
+    this.create();
+  }
+
+  create() {
+    this._domController.createDom(`<div class="cat">길냥이</div>`);
+    this._domController.setEvent("cat");
+  }
+
+  getName() {
+    return this._dataController.getProfile().name;
+  }
+
+  callState() {
+    const state = this._dataController.getState();
+    trigger("onCatClick", { state });
+  }
+
+  callProfile() {
+    const info = this._dataController.getProfile();
+    trigger("onCatHover", { info });
+  }
+
+  fireProfile() {
+    trigger("onCartLeave");
+  }
+
+  call() {
+    console.log("그냥 온다.");
   }
 }
 
 class CatData {
-  constructor({ name, age, state }) {
-    this.name = name;
-    this.age = age;
+  constructor({ profile, state }) {
+    this.profile = profile;
     this.state = state;
     setInterval(this.randomAct.bind(this), CAT_RANDOM_TIMER);
   }
 
   getProfile() {
-    return {
-      name: this.name,
-      age: this.age,
-    };
+    return this.profile;
   }
 
   getState() {
@@ -90,19 +130,23 @@ class CatData {
 }
 
 class CatDom {
-  constructor($parent, dataController) {
+  constructor($parent, event) {
     this.$parent = $parent;
-    this.dataController = dataController;
-    this.createDom();
-    this.setEvent();
+    this.event = event;
   }
 
-  createDom() {
-    this.$parent.innerHTML += `<div class="cat">고양이</div>`;
+  createDom(template) {
+    const [, , , , className, , text] = template.split(/<|>|"|div|=/);
+    const node = document.createElement("div");
+    node.classList.add(className);
+    node.innerText = text;
+    this.$parent.append(node);
   }
 
-  setEvent() {
-    const $targetEl = Array.from(this.$parent.getElementsByClassName("cat"));
+  setEvent(targetClassName) {
+    const $targetEl = Array.from(
+      this.$parent.getElementsByClassName(targetClassName)
+    );
     $targetEl.forEach(($el) => {
       if (!$el) return;
       $el.addEventListener("click", () => {
@@ -118,17 +162,127 @@ class CatDom {
   }
 
   click() {
-    const state = this.dataController.getState();
-    trigger("onCatClick", { state });
+    this.event.callState();
   }
 
   hover() {
-    const info = this.dataController.getProfile();
-    trigger("onCatHover", { info });
+    this.event.callProfile();
   }
 
   leave() {
-    trigger("onCartLeave");
+    this.event.fireProfile();
+  }
+}
+
+class RussianBlue extends Cat {
+  constructor($parent, data) {
+    super($parent, {
+      ...data,
+      type: "러시안 블루",
+    });
+    this._dataController;
+  }
+
+  /*
+   * @override
+   */
+  create() {
+    this._domController.createDom(
+      `<div class="russianBlueCat">러시안 블루</div>`
+    );
+    this._domController.setEvent("russianBlueCat");
+  }
+
+  /*
+   * @override
+   */
+  call() {
+    console.log("원을 2번 그리고 나에게 온다");
+  }
+}
+
+class koreanShorthairCat extends Cat {
+  constructor($parent, data) {
+    super($parent, {
+      ...data,
+      type: "코숏",
+    });
+    this._dataController;
+  }
+
+  /*
+   * @override
+   */
+  create() {
+    this._domController.createDom(`<div class="koreanSortHair">코숏</div>`);
+    this._domController.setEvent("koreanSortHair");
+  }
+
+  /*
+   * @override
+   */
+  call() {
+    console.log("물끄러미 바라만 본다");
+  }
+}
+
+class BengalCat extends Cat {
+  #callCount;
+
+  constructor($parent, data) {
+    super($parent, {
+      ...data,
+      type: "뱅갈",
+    });
+    this._dataController;
+    this.#callCount = 0;
+  }
+
+  /*
+   * @override
+   */
+  create() {
+    this._domController.createDom(`<div class="bengalCat">뱅갈</div>`);
+    this._domController.setEvent("bengalCat");
+  }
+
+  /*
+   * @override
+   */
+  call() {
+    this.#callCount += 1;
+    if (this.#callCount >= 2) {
+      console.log("나에게 온다.");
+    } else {
+      console.log("2번 이상 불러야 나에게 온다");
+    }
+  }
+}
+
+class NorwegianForestCat extends Cat {
+  constructor($parent, data) {
+    super($parent, {
+      ...data,
+      type: "노르웨이숲",
+    });
+    this._dataController;
+  }
+
+  /*
+   * @override
+   */
+  create() {
+    this._domController.createDom(
+      `<div class="norwegianForestCat">노르웨이숲</div>`
+    );
+    this._domController.setEvent("norwegianForestCat");
+  }
+
+  /*
+   * @override
+   */
+  call() {
+    console.log("박스 속으로 들어간다.");
   }
 }
 
@@ -142,9 +296,11 @@ class InfoToolTip {
   setEvent() {
     addEventListener("onCatHover", (e) => {
       const { info } = e.detail;
-      const { name, age } = info;
+      const { name, age, type } = info;
       this.$el.classList.remove("hide");
-      this.$el.innerHTML = `이름: ${name}, 나이: ${age}`;
+      this.$el.innerHTML = `이름: ${name}, 나이: ${age} ${
+        type ? `종류: ${type}` : ""
+      }`;
     });
 
     addEventListener("onCartLeave", (e) => {
@@ -169,6 +325,73 @@ class StateToolTip {
       }, 700);
     });
   }
+}
+
+class callCatBtn {
+  constructor(clickFunc) {
+    this.$el = document.getElementById("catCallBtn");
+    this.clickFunc = clickFunc;
+    this.setEvent();
+  }
+
+  setEvent() {
+    this.$el.addEventListener("click", () => {
+      this.clickFunc();
+    });
+  }
+}
+
+// makeCatButton
+class makeCatButton {
+  #result;
+  #maxNum;
+
+  constructor() {
+    this.$el = document.getElementById("makeCatBtn");
+    this.#result = [];
+    this.#maxNum = 6;
+    this.#setEvent();
+  }
+
+  getCatBox() {
+    return this.#result.map((catClass) => {
+      return catClass.getName();
+    });
+  }
+
+  getCatClass(name) {
+    const catNameList = this.getCatBox();
+    const idx = catNameList.findIndex((catName) => catName === name);
+    return this.#result[idx];
+  }
+
+  #setEvent() {
+    this.$el.addEventListener("click", () => {
+      if (this.#result.length === this.#maxNum) {
+        alert("고양이를 더 이상 추가할 수 없어요!!!");
+      } else {
+        this.#result.push(makeCat());
+      }
+    });
+  }
+
+  click() {
+    this.$el.click();
+  }
+}
+
+function callCat(makeCatClass) {
+  const $input = document.getElementById("catCallName");
+
+  return function () {
+    const name = $input.value;
+    if (name === "") return alert("이름을 입력해주세요");
+    const nameList = makeCatClass.getCatBox();
+    if (!nameList.includes(name))
+      return alert("고양이 이름을 다시 확인해주세요.");
+    const catClass = makeCatClass.getCatClass(name);
+    catClass?.call();
+  };
 }
 
 main();
